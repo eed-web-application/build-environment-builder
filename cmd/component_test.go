ebaf3f357b2a0a35c8fdb9352f11d3faab662d94
Claudio Bisegni <bisegni@slac.stanford.edu>
2024-05-14 17:38:38 +0000

    fix

--- cmd/component_test.go
+++ cmd/component_test.go
@@ -17,26 +17,6 @@ func TestComponent(t *testing.T) {
 	assert.NoError(t, err)
 }
 
-func TestComponentFindAllfailNoLabel(t *testing.T) {
-	buf := new(bytes.Buffer)
-	rootCmd.SetOut(buf)
-	rootCmd.SetErr(buf)
-	rootCmd.SetArgs([]string{"component", "find-all"})
-
-	err := rootCmd.Execute()
-	assert.Error(t, err)
-}
-
-func TestComponentFindAllfailLabelNotFound(t *testing.T) {
-	buf := new(bytes.Buffer)
-	rootCmd.SetOut(buf)
-	rootCmd.SetErr(buf)
-	rootCmd.SetArgs([]string{"component", "find-all", "--label", "notfound"})
-
-	err := rootCmd.Execute()
-	assert.Error(t, err)
-}
-
 func TestComponentFindAllOK(t *testing.T) {
 	Configuration.Endpoints = make(map[string]string)
 	Configuration.Endpoints["test-1"] = "http://cbs:8080"
